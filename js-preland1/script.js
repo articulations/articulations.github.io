46de420c04692e89a11919e9093d8ba45134d0df
Your Name <you@example.com>
2021-12-09 11:32:28 +0300

    added

--- js-preland1/script.js
+++ js-preland1/script.js
@@ -9,10 +9,3 @@ $('.close').click(function () {
     $('.popup').fadeOut();
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    ymaps.ready(function () {
-        var e, n = ymaps.geolocation,
-            t = document.getElementsByClassName("user-city");
-        for (e = 0; e < t.length; e++) t[e].innerHTML = " " + n.city
-    })
-});
